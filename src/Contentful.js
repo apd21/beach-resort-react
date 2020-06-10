63aa5828582fc5a43e78a416d7de962aaddfe6a8
Apdet <deterding1.a@gmail.com>
2020-06-10 17:07:31 -0500

    added file to gitignore

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,7 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-  space: "8ymizqjvplgo",
-  accessToken:"mXaYE0_tl69rq6h6VlByN6U9Z64XiWyw_oqsqSrVVrY"
-
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
